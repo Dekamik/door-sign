96f2d00d7ab25ca9a714eeebae320a214b79b491
Dennis von Bargen <dennis@vonbargen.se>
2023-07-22 22:21:04 +0200

    Added progress from old project

--- door-sign.go
+++ door-sign.go
@@ -17,13 +17,20 @@ func main() {
 	r.GET("/", func(c *gin.Context) {
 		t := template.Must(template.ParseFS(templateFS,
 			"templates/index.html",
-			"templates/htmx_time.html"))
+			"templates/htmx_sl.html",
+			"templates/htmx_yr.html"))
+		t.Execute(c.Writer, nil)
+	})
+
+	r.GET("/htmx/sl.html", func(c *gin.Context) {
+		t := template.Must(template.ParseFS(templateFS,
+			"templates/htmx_sl.html"))
 		t.Execute(c.Writer, gin.H{"ts": timeNow()})
 	})
 
-	r.GET("/htmx/time.html", func(c *gin.Context) {
+	r.GET("/htmx/yr.html", func(c *gin.Context) {
 		t := template.Must(template.ParseFS(templateFS,
-			"templates/htmx_time.html"))
+			"templates/htmx_yr.html"))
 		t.Execute(c.Writer, gin.H{"ts": timeNow()})
 	})
 
